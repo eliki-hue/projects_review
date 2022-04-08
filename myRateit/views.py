2cb47b0fb4110be498d7920f32b4e91d509cdba8
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-08 20:04:37 +0300

    created home view

--- myRateit/views.py
+++ myRateit/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    message ='Welcome to rateit'
+    return render(request, 'index.html',{'message': message})
