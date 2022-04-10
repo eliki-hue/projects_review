7c1fe1d343d7dc629dc89ca874838cec3138c153
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-10 23:39:33 +0300

    added main.js in static folder

---
+++ static/main.js
@@ -0,0 +1,9 @@
+console.log('hellow js')
+
+//get all the stars
+
+const one= document.getElementById('first')
+const two= document.getElementById('second')
+const three= document.getElementById('three')
+const four= document.getElementById('four')
+const five= document.getElementById('five')
