0daebb11579e0d53268f5400b01b7cc6047e2b8e
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-10 19:49:27 +0300

    added project_detail url

--- myRateit/urls.py
+++ myRateit/urls.py
@@ -10,6 +10,8 @@ urlpatterns=[
     path('my_post', views.add_post, name='my_post'),
     path('add_comment', views.add_comment, name='add_comment'),
     path('signup', views.signup, name='signup'),
-    path("like/<int:id>",views.project_like,name='project_like')
+    path("like/<int:id>",views.project_like,name='project_like'),
+    path("details/<int:id>",views.project_details,name='project_details')
+
     
 ]
