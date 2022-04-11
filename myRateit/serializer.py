bd5177d9c51180e207d51126bfa98c38dd7122df
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-11 21:08:18 +0300

    created profile serializer

--- myRateit/serializer.py
+++ myRateit/serializer.py
@@ -5,3 +5,8 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
